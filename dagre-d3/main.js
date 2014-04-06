493b7ef1799657302cdbc7683221be560fd036bd
Christof Sirk <christof.sirk@gmail.com>
2014-04-06 16:41:09 +0200

    Change example sentence and add edges.

--- dagre-d3/main.js
+++ dagre-d3/main.js
@@ -1,10 +1,14 @@
 var g = new dagreD3.Digraph();
 
-g.addNode("0",    { label: "Cogito" });
-g.addNode("1",    { label: "ergo" });
-g.addNode("2",    { label: "sum" });
+g.addNode("0",    { label: "0" });
+g.addNode("1",    { label: "Marcus" });
+g.addNode("2",    { label: "rosam" });
+g.addNode("3",    { label: "videt" });
 
-//g.addEdge(null, "0",  "1", { label: "K-PAX" });
+// g.addEdge(null, "0",  "1", { label: "K-PAX" });
+g.addEdge(null, "0", "3", { label: "PRED" });
+g.addEdge(null, "3", "1", { label: "SUBJ" });
+g.addEdge(null, "3", "2", { label: "PRED" });
 
 var renderer = new dagreD3.Renderer();
 
